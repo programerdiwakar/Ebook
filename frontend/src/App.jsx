40aa1cb42d58cc275ae4e2967ebfa45d9aff75ed
Diwakar <singhdiwakar3208@gmail.com>
2025-11-05 16:12:30 +0530

    hero done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Home from './pages/Home.jsx'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx'
+import AllBooks from './pages/AllBooks.jsx'
+import Profile from './pages/Profile.jsx'
+import Cart from './pages/Cart.jsx'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Home />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route  path="/all-books" element={<AllBooks/>} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route  path="/signUp" element={<SignUp/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route  path="/profile" element={<Profile/>} />
+        </Routes>
+        <Footer />
+
+      </Router>
+      
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const links = [
         {
             title: "Home",
             link: "/"
-        },
-        {
-            title: 'About Us',
-            link: '/about-us'
         },
         {
             title: 'All Books',
@@ -26,20 +23,20 @@ const Navbar = () => {
     return (
         <div className='bg-zinc-800 px-8 py-2 text-white flex justify-between items-center'>
 
-            <div className='flex items-center '>
+            <Link to='/' className='flex items-center '>
                 <img src="./images/logo.png" alt="" className='h-16 pt-2 ' />
                 <h1 className='text-2xl font-semibold'>BookNest</h1>
-            </div>
+            </Link>
 
             <div className='nav-links-booknest  flex  items-center gap-2'>
                 <div className='flex gap-4'>
                     {links.map((items, i) => (
-                        <div key={i} className='hover:text-blue-500 transition-all duration-300'>{items.title}</div>
+                        <Link to={items.link} key={i} className='hover:text-blue-500 transition-all duration-300'>{items.title}</Link>
                     ))}
                 </div>
                 <div className='flex gap-4'>
-                    <button className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignIn</button>
-                    <button className=' px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignUp</button>
+                    <Link to='/signIn' className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignIn</Link>
+                    <Link to='/signUp' className=' px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignUp</Link>
                 </div>
 
             </div>
