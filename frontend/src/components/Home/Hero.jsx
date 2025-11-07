d9e80baad48c89c312f49a065e6902d6fa326516
Diwakar <singhdiwakar3208@gmail.com>
2025-11-07 11:17:42 +0530

    making navbar and hero responsive

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
     return (
-        <div className='h-[75vh] flex'>
-            <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
+        <div className='h-[75vh] flex flex-col md:flex-row items-center justify-center'>
+            <div className='w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
                 <h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>
                     Books That Make You Feel Seen
                 </h1>
@@ -11,11 +12,11 @@ const Hero = () => {
                     Explore timeless tales, insightful reads,
                     and boundless imagination—all handpicked for curious minds.
                 </p>
-                <div className='mt-8'>
+                <Link to='/all-books' className='mt-8'>
                     <button className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
                     Explore the Collection
                     </button>
-                </div>
+                </Link>
                 
             </div>
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
     const links = [
@@ -20,27 +21,57 @@ const Navbar = () => {
             link: '/profile'
         },
     ];
+    const [MobileNav, setMobileNav] = useState("hidden");
     return (
-        <div className='bg-zinc-800 px-8 py-2 text-white flex justify-between items-center'>
+        <>
+            <nav className='bg-zinc-800 px-8 py-2 text-white relative z-50 flex justify-between items-center'>
 
-            <Link to='/' className='flex items-center '>
-                <img src="./images/logo.png" alt="" className='h-16 pt-2 ' />
-                <h1 className='text-2xl font-semibold'>BookNest</h1>
-            </Link>
+                <Link to='/' className='flex items-center '>
+                    <img src="./images/logo.png" alt="" className='h-16 pt-2 ' />
+                    <h1 className='text-2xl font-semibold'>BookNest</h1>
+                </Link>
+                <div className='nav-links-booknest block md:flex  items-center gap-2'>
+                    <div className=' hidden md:flex gap-4'>
+                        {links.map((items, i) => (
+                            <Link to={items.link} key={i} className='hover:text-blue-500 transition-all duration-300'>{items.title}</Link>
+                        ))}
+                    </div>
 
-            <div className='nav-links-booknest  flex  items-center gap-2'>
-                <div className='flex gap-4'>
-                    {links.map((items, i) => (
-                        <Link to={items.link} key={i} className='hover:text-blue-500 transition-all duration-300'>{items.title}</Link>
-                    ))}
+                    <div className=' hidden md:flex gap-4'>
+                        <Link to='/signIn' className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignIn</Link>
+                        <Link to='/signUp' className=' px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignUp</Link>
+                    </div>
+                    <button className='text-white text-2xl hover:text-zinc-400' onClick={() => (MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden"))}>
+                        <GiHamburgerMenu />
+                    </button>
                 </div>
-                <div className='flex gap-4'>
-                    <Link to='/signIn' className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignIn</Link>
-                    <Link to='/signUp' className=' px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>SignUp</Link>
-                </div>
-
+            </nav>
+            <div className={`${MobileNav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center`}>
+                {links.map((items, i) => (
+                    <Link
+                        to={items.link}
+                        className={` ${MobileNav} text-white text-4xl mb-10 font-semibold hover:text-blue-500 transition-all duration-300`}
+                        key={i}
+                        onClick={() => (MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden"))}
+                    >
+                        {items.title}{" "}
+                    </Link>
+                ))}
+                <Link
+                    to='/signIn' 
+                    className={`${MobileNav} px-4 py-2 mb-10 border text-3xl font-semibold text-white border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}
+                    onClick={() => (MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden"))}
+                >
+                    SignIn
+                </Link>
+                <Link to='/signUp' className={`${MobileNav} px-4 py-2 mb-10 text-3xl font-semibold text-white bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-3001`}
+                    onClick={() => (MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden"))}
+                >
+                    SignUp
+                </Link>
             </div>
-        </div>
+        </>
+
     )
 }
 
