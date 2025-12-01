f4aff10fdd52db708790d22e2fff31ea9720f297
Diwakar <singhdiwakar3208@gmail.com>
2025-12-01 11:44:28 +0530

    Cart functionality is done

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -24,15 +24,17 @@ router.put('/add-to-cart', authenticateToken, async (req, res) => {
 });
 
 // remove book from cart
-router.delete('/remove-from-cart', authenticateToken, async (req, res) => {
+router.put('/remove-from-cart/:bookid', authenticateToken, async (req, res) => {
     try {
-        const { bookid, id } = req.headers;
-        const userData = await User.findById(id);
-        const isBookInCart = userData.cart.includes(bookid);
-        if (isBookInCart) {
-            await User.findByIdAndUpdate(id, { $pull: { cart: bookid } });
-        }
-        return res.status(200).json({message:'Book removed from cart successfully'})
+        const { bookid } = req.params;
+        const { id } = req.headers;
+        await User.findByIdAndUpdate(id, {
+            $pull:{cart:bookid},
+        })
+        return res.json({
+            status: "success",
+            message: 'Book removed from cart successfully'
+        })
     }
     catch (err) {
         console.log(err);
